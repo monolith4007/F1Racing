b896ff5d3ae26ea940ceff8f9300c20edb600145
monolith4007 <118559622+monolith4007@users.noreply.github.com>
2024-12-06 21:03:24 +0000

    Added read_* functions

--- F1Racing.py
+++ F1Racing.py
@@ -1,1 +1,17 @@
-print("Hello world!")
+# Reader functions
+def read_results():
+    file = open("results.txt", "r")
+    str = file.read()
+    file.close()
+    return str
+    
+results_string = read_results()
+print(results_string)
+
+def read_drivers():
+    file = open("drivers.txt", "r")
+    str = file.read()
+    file.close()
+    return str
+
+# Good start so far...
