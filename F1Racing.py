739f0e4cb47850d2e20abf6be5bc3afc6724f320
monolith4007 <118559622+monolith4007@users.noreply.github.com>
2024-12-07 21:24:34 +0000

    Began writing results functions
    Started off with individual_race_result.

--- F1Racing.py
+++ F1Racing.py
@@ -1,12 +1,9 @@
-# Reader functions
+# --- Reader functions ---
 def read_results():
     file = open("results.txt", "r")
     str = file.read()
     file.close()
     return str
-    
-results_string = read_results()
-print(results_string)
 
 def read_drivers():
     file = open("drivers.txt", "r")
@@ -14,4 +11,21 @@ def read_drivers():
     file.close()
     return str
 
-# Good start so far...
+# --- Results functions ---
+def individual_race_result(results_string, drivers_string, race_number):
+    # Validate race
+    if race_number < 1 or race_number > 2:
+        print(f"No results available for Race {race_number}")
+        return
+
+    # Assign each result to a list
+    results_list = results_string.splitlines()
+    print(results_list)
+
+    # Assign each driver to a list
+    drivers_list = drivers_string.splitlines()
+    print(drivers_list)
+        
+    
+
+individual_race_result(read_results(), read_drivers(), 1)
